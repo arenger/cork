5c0053f7857f3cd76c2904953fadeff0a3a1bf30
Alex Renger <arenger@gmail.com>
2014-09-12 12:39:39 -0400

    sample business logic

---
+++ src/main/java/com/example/biz/Digester.java
@@ -0,0 +1,36 @@
+package com.example.biz;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.dto.Message;
+
+public class Digester {
+   private static final Logger LOG
+      = LoggerFactory.getLogger(Digester.class);
+
+   public String getMd5(List<Message> messages) {
+      if ((messages == null) || (messages.size() == 0)) {
+         return "";
+      }
+      String hash = "error";
+      try {
+         MessageDigest md = MessageDigest.getInstance("MD5");
+         StringBuilder sb = new StringBuilder();
+         for (Message m : messages) {
+            sb.append(m.getContent());
+         }
+         BigInteger bi = new BigInteger(1,
+            md.digest(sb.toString().getBytes()));
+         hash = bi.toString(16);
+      } catch (NoSuchAlgorithmException e) {
+         LOG.error("MD5 DNE.  That's odd.");
+      }
+      return hash;
+   }
+}
--- src/main/java/com/example/rest/MessageService.java
+++ src/main/java/com/example/rest/MessageService.java
@@ -6,10 +6,12 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.biz.Digester;
 import com.example.dal.DalException;
 import com.example.dal.MessageKeeper;
 import com.example.dto.Message;
@@ -35,13 +37,13 @@ public class MessageService {
    }
 
    @GET
-   @Path("getFrom")
+   @Path("getFrom/{personId}")
    @Consumes("text/plain")
-   public List<Message> getFrom(String personId) {
+   public List<Message> getFrom(@PathParam("personId") int personId) {
       List<Message> ret = null;
       try {
          MessageKeeper mk = new MessageKeeper();
-         ret = mk.getMessagesFrom(Integer.parseInt(personId.trim()));
+         ret = mk.getMessagesFrom(personId);
       } catch (DalException e) {
          LOG.error("Problem getting messages from {}", personId, e);
       }
@@ -49,16 +51,31 @@ public class MessageService {
    }
 
    @GET
-   @Path("getTo")
+   @Path("getTo/{personId}")
    @Consumes("text/plain")
-   public List<Message> getTo(String personId) {
+   public List<Message> getTo(@PathParam("personId") int personId) {
       List<Message> ret = null;
       try {
          MessageKeeper mk = new MessageKeeper();
-         ret = mk.getMessagesTo(Integer.parseInt(personId.trim()));
+         ret = mk.getMessagesTo(personId);
       } catch (DalException e) {
          LOG.error("Problem getting messages from {}", personId, e);
       }
       return ret;
+   }
+
+   @GET
+   @Path("md5/sentFrom/{personId}")
+   @Consumes("text/plain")
+   public String md5from(@PathParam("personId") int personId) {
+      String md5 = null;
+      try {
+         MessageKeeper mk = new MessageKeeper();
+         Digester d = new Digester();
+         md5 = d.getMd5(mk.getMessagesFrom(personId)) + "\n";
+      } catch (DalException e) {
+         LOG.error("Problem getting messages from {}", personId, e);
+      }
+      return md5;
    }
 }
